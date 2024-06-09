e968280d02b49d07ccb0333d88a9cd99f90df06a
JeffGuo1432 <JeffDaMan1432@gmail.com>
2024-06-08 22:48:40 -0400

    Added XP

--- FinalGame/MyWorld.java
+++ FinalGame/MyWorld.java
@@ -6,27 +6,37 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MyWorld extends World
-{
+public class MyWorld extends World {
+    public static final int WORLD_WIDTH = 1048;
+    public static final int WORLD_HEIGHT = 800;
     private Character c;
     private Gun g;
     private int speed;
+    private int actCount = 0;
+
     /**
      * Constructor for objects of class MyWorld.
-     * 
      */
-    public MyWorld()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+    public MyWorld() {
+        // Call the superclass constructor with the constants
+        super(WORLD_WIDTH, WORLD_HEIGHT, 1);
+        setPaintOrder(Character.class,Collectible.class);
         c = new Character();
         g = new Gun();
         addObject(c, 300, 200);
         addObject(g, 280, 180);
         speed = 2;
     }
+
     
     public void act(){
+        actCount++;
+        /**
+         * Randomly spawn xp for now
+         */
+        if(actCount%5==0){
+            addObject(new XP(),Greenfoot.getRandomNumber(WORLD_WIDTH),Greenfoot.getRandomNumber(WORLD_HEIGHT));
+        }
     }
     
     public Character getCharacter(){
---
+++ FinalGame/XP.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class XP here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class XP extends Collectible
+{
+    /**
+     * Act - do whatever the XP wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public XP(){
+        super(20,60);
+    }
+    
+    public void pickupEffect(){
+        
+    }
+}
